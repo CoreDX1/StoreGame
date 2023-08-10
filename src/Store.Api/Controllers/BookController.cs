f56d2d4a513b29f7b3732ea2b6cdb2f835d74aeb
CoreDX1 <chrismaquis@gmail.com>
2023-08-10 16:02:50 -0300

    Creating Book

---
+++ src/Store.Api/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Api;
+
+public class BookController : ControllerBase
+{
+    [HttpGet("/books")]
+    public ActionResult<string> Message()
+    {
+        return "Hello, World!";
+    }
+}
